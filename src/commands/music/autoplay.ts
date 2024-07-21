76c27b886372a9d9b92b10534252912ffa432222
Jirayu <133365968+JirayuSrisawat-Github@users.noreply.github.com>
2024-07-22 02:54:32 +0700

    Update autoplay.ts

--- src/commands/music/autoplay.ts
+++ src/commands/music/autoplay.ts
@@ -1,27 +1,27 @@
 export default {
-    name: "autoplay",
-    description: "Set autoplay",
-    category: "music",
-    run: async ({ client, interaction }: ICommandOptions) => {
-        const player = client.manager.players.get(interaction.guildId!);
-        if (!player || !player?.queue?.current)
-            return await interaction.reply({
-                embeds: [
-                    {
-                        color: 0xffffff,
-                        description: "No music playing in this server!",
-                    },
-                ],
-            });
+	name: "autoplay",
+	description: "Set autoplay",
+	category: "music",
+	run: async ({ client, interaction }: ICommandOptions) => {
+		const player = client.manager.players.get(interaction.guildId!);
+		if (!player || !player?.queue?.current)
+			return await interaction.reply({
+				embeds: [
+					{
+						color: 0xffffff,
+						description: "No music playing in this server!",
+					},
+				],
+			});
 
-        player.setAutoplay(true, interaction.user);
-        return await interaction.reply({
-            embeds: [
-                {
-                    color: 0xffffff,
-                    description: "Autoplay is on!",
-                },
-            ],
-        });
-    },
+		player.setAutoplay(!player.isAutoplay);
+		return await interaction.reply({
+			embeds: [
+				{
+					color: 0xffffff,
+					description: `Autoplay is ${player.isAutoplay ? "off" : "on"}!`,
+				},
+			],
+		});
+	},
 } as ICommand;
